53b917c14c02de9e29b9dc75d6b4698d5b8fcf21
defini7 <alexander.suhov@hotmail.com>
2024-10-17 16:21:12 +0300

    Revert "added mstring_join_cstr, fixed mstring_convert"
    This reverts commit 9677910a70de44ce4d5fcf1b80786dce4fa374a3.

--- examples/join.c
+++ examples/join.c
@@ -1,20 +1,18 @@
 #define MSTRING_IMPL
 #include "../mstring.h"
 
-double mstring_into_double(mstring str)
-{
-    return DEREF(double, mstring_convert(str, "%lf", sizeof(double)));
-}
-
 int main()
 {
-    mstring before_point = mstring_new("232");
-    mstring numeric = mstring_join_cstr(before_point, ".2131");
-    
-    printf("%lf\n", mstring_into_double(numeric));
+    mstring hello = mstring_new("Hello, ");
+    mstring world = mstring_new("world!");
 
-    mstring_free(before_point);
-    mstring_free(numeric);
+    mstring hello_world = mstring_join(hello, world);
+
+    printf("%s\n", mstring_raw(hello_world));
+
+    mstring_free(hello);
+    mstring_free(world);
+    mstring_free(hello_world);
 
     return 0;
 }
